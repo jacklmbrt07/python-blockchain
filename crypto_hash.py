281fb54b737f63d536ed11d7574ef0faf444fd17
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-05 16:30:45 -0800

    added crypto_hash algo

---
+++ crypto_hash.py
@@ -0,0 +1,15 @@
+import hashlib
+import json
+
+def crypto_hash(data):
+    """
+    Return a sha-256 hash of the given data
+    """
+    stringified_data = json.dumps(data)
+    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+
+def main():
+    print(f"crypto_hash(): {crypto_hash([2, 4])}")
+
+if __name__ == '__main__':
+    main()
