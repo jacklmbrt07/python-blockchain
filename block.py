9425e388efce2972e56eb7eff19548bc5514d722
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-05 16:49:30 -0800

    added second part of hashing algo

--- block.py
+++ block.py
@@ -1,5 +1,7 @@
 import time 
 
+from crypto_hash import crypto_hash
+
 class Block:
     """
     Block: a unit of storage
@@ -27,7 +29,7 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = f'{timestamp}-{last_hash}'
+        hash = crypto_hash(timestamp, last_hash, data)
     
         return Block(timestamp, last_hash, hash, data)
 
--- crypto_hash.py
+++ crypto_hash.py
@@ -1,15 +1,17 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def crypto_hash(*args):
     """
-    Return a sha-256 hash of the given data
+    Return a sha-256 hash of the given arguments.
     """
-    stringified_data = json.dumps(data)
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = ''.join(stringified_args)
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash(): {crypto_hash([2, 4])}")
+    print(f"crypto_hash('one', 2 , [3]): {crypto_hash('one', 2, [3])}")
 
 if __name__ == '__main__':
     main()
