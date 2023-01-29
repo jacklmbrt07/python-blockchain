c492930cb0da5f2a89cd4170afbc7473255a28e7
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-29 08:00:03 -0800

    added hex_to_binary tests

--- backend/util/hex_to_binary.py
+++ backend/util/hex_to_binary.py
@@ -1,3 +1,5 @@
+from backend.util.crypto_hash import crypto_hash
+
 HEX_TO_BINARY_CONVERSION_TABLE = {
     '0': '0000',
     '1': '0001',
@@ -16,3 +18,28 @@ HEX_TO_BINARY_CONVERSION_TABLE = {
     'e': '1110',
     'f': '1111',
 }
+
+def hex_to_binary(hex_string):
+    binary_string = ''
+
+    for character in hex_string:
+        binary_string += HEX_TO_BINARY_CONVERSION_TABLE[character]
+
+    return binary_string
+
+def main():
+    number = 451
+    hex_number = hex(number)[2:]
+    print(f'hex_number: {hex_number}')
+
+    binary_number = hex_to_binary(hex_number)
+    print(f'binary_number: {binary_number}')
+
+    original_number = int(binary_number, 2)
+    print(f'original_number: {original_number}')
+
+    hex_to_binary_crypto_hash = hex_to_binary(crypto_hash('test-data'))
+    print(f'hex_to_binary_crypto_hash: {hex_to_binary_crypto_hash}')
+
+if __name__ == '__main__':
+    main()
