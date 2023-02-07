4c75a643fb7648b8387954f2d8f83a33efa0790f
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-02-07 10:33:06 -0800

    added pubsub initiaion

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -7,22 +7,36 @@ from pubnub.callbacks import SubscribeCallback
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-e9af7018-6480-4235-88da-30d51b145b49'
 pnconfig.publish_key = 'pub-c-4b50a9c5-642a-4efe-90e5-1a092dcfddce'
-pubnub = PubNub(pnconfig)
 
 TEST_CHANNEL = 'TEST_CHANNEL'
 
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
-
 class Listener(SubscribeCallback):
     def message(self, pubnub, message_object):
-        print(f'\n-- Incoming message_object: {message_object}')
+        print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
 
-pubnub.add_listener(Listener())
+class PubSub():
+    """
+    Handles the publish/subscribe layer of the application
+    Provides communication between the nodes of the blockchain network.
+    """
+    def __init__(self):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        """
+        Publish the message object to the channel.
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
+
 
 def main():
+    pubsub = PubSub()
+
     time.sleep(1)
     
-    pubnub.publish().channel(TEST_CHANNEL).message({ 'foo': 'bar' }).sync()
+    pubsub.publish(TEST_CHANNEL, { 'foo': 'bar' })
 
 if __name__ == '__main__':
     main()
